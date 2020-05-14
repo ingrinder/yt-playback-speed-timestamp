68d18360334001c8d2de4b4c89eaac68196c871e
Michael Bromilow <12384431+ingrinder@users.noreply.github.com>
2020-05-14 00:17:05 +0000

    Tweak various little bits
    * Use `let` instead of `var`
    * Attempt to be a bit more modular...

--- script.js
+++ script.js
@@ -1,51 +1,49 @@
-function secondsToDhms(secs) {
-    var d = Math.floor(secs / (3600*24));
-    var h = Math.floor(secs % (3600*24) / 3600);
-    var m = Math.floor(secs % 3600 / 60);
-    var s = Math.floor(secs % 60);
-    var dDisplay = d > 0 ? d + (':') : "";
-    var hDisplay = (h > 0 || d > 0) ? (dDisplay === "" ? h : ("00" + h).slice(-2)) + (':') : "";
-    var mDisplay = ("00" + m).slice(-2) + (':');
-    var sDisplay = ("00" + s).slice(-2);
+function getSecsAsDHMS(secs) {
+    let d = Math.floor(secs / (3600*24));
+    let h = Math.floor(secs % (3600*24) / 3600);
+    let m = Math.floor(secs % 3600 / 60);
+    let s = Math.floor(secs % 60);
+    let dDisplay = d > 0 ? d + (':') : "";
+    let hDisplay = (h > 0 || d > 0) ? (dDisplay === "" ? h : ("00" + h).slice(-2)) + (':') : "";
+    let mDisplay = ("00" + m).slice(-2) + (':');
+    let sDisplay = ("00" + s).slice(-2);
     return dDisplay + hDisplay + mDisplay + sDisplay;
 }
-function newTimeElapsedCalc() {
-    var seconds = ytplayer = document.getElementById("movie_player").getCurrentTime();
-    var speed = document.getElementsByClassName('html5-main-video')[0].playbackRate;
-    var newSeconds = Math.round(seconds / speed);
-    var newTimeText = secondsToDhms(newSeconds);
-    return newTimeText;
+
+function getAdjustedSecs(secs) {
+    let speed = document.getElementsByClassName('html5-main-video')[0].playbackRate;
+    return Math.round(secs / speed);
 }
-function newTimeDurationCalc() {
-    var seconds = ytplayer = document.getElementById("movie_player").getDuration();
-    var speed = document.getElementsByClassName('html5-main-video')[0].playbackRate;
-    var newSeconds = Math.round(seconds / speed);
-    var newTimeText = secondsToDhms(newSeconds);
-    return newTimeText;
+
+function getNewTimeString(currSpeed) {
+    let currSecs = document.getElementById("movie_player").getCurrentTime();
+    let currDuration = document.getElementById("movie_player").getDuration();
+    let newTimeElapsed = getSecsAsDHMS(Math.round(currSecs / currSpeed));
+    let newTimeDuration = getSecsAsDHMS(Math.round(currDuration / currSpeed));
+    return " (" + newTimeElapsed + " / " + newTimeDuration + ")";
 }
-function writeNewTime() {
-    var currSpeed = document.getElementsByClassName('html5-main-video')[0].playbackRate;
-    var newTimeElement = document.getElementById("new-speed-time");
+
+function updateNewTime() {
+    let currSpeed = document.getElementsByClassName('html5-main-video')[0].playbackRate;
+    let newTimeElement = document.getElementById("ytp-time-speed-adjusted");
     if (currSpeed === 1) {
         if (newTimeElement !== null) {
             newTimeElement.remove();
         }
         return;
     }
-    var newTimeElapsed = newTimeElapsedCalc();
-    var newTimeDuration = newTimeDurationCalc();
-    var newTime = " (" + newTimeElapsed + " / " + newTimeDuration + ")";
     if (newTimeElement === null) {
-        var node = document.createElement("span");
-        node.setAttribute("id", "new-speed-time");
-        node.innerHTML = newTime;
+        let node = document.createElement("span");
+        node.setAttribute("id", "ytp-time-speed-adjusted");
+        node.innerHTML = getNewTimeString(currSpeed);
         document.getElementsByClassName("ytp-time-display")[0].appendChild(node);
     }
     else {
-        newTimeElement.innerHTML = newTime;
+        newTimeElement.innerHTML = getNewTimeString(currSpeed);
     }
 }
+
 interval = setInterval(function() {
-   writeNewTime();
+   updateNewTime();
  }, 500
 );
